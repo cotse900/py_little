3cb9871fba3885eea8ef392455816edcd251a5b2
Chungon <cotse900@gmail.com>
2022-07-06 12:23:19 -0400

    Create basics.py

---
+++ basics.py
@@ -0,0 +1,13 @@
+# https://stackoverflow.com/questions/8908760/should-i-use-camel-case-or-underscores-in-python
+# start off!
+print("hello")
+#
+# import webbrowser
+# webbrowser.open("https://github.com")
+#
+print(1+2)
+
+a = 1
+
+print(type(a))
+# <class 'int'>
